3c35680106c4d41a2142bcfcdd440b8a8b003c42
yasirabd <sir.yasirabd@gmail.com>
2025-07-20 12:57:09 +0700

    chore: change use_finetuned to False

--- app/services/openai_client.py
+++ app/services/openai_client.py
@@ -8,7 +8,7 @@ model_config = load_yaml_config("config/model.yaml")
 BASE_MODEL = model_config["openai"]["base_model"]
 FINETUNED_MODEL = model_config["openai"]["finetuned_model"]
 
-def generate_chat_reply(messages, model=None, use_finetuned=True):
+def generate_chat_reply(messages, model=None, use_finetuned=False):
     selected_model = model or (FINETUNED_MODEL if use_finetuned else BASE_MODEL)
     
     body = {
